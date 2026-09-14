a47390dfc64dd3e7575e476e9f1ea0792ff19987
Diana Bagdasaryan <bagdasarandiana953@gmail.com>
2026-09-14 12:40:00 +0700

    новый

--- src/main/java/com/ProjectController.java
+++ src/main/java/com/ProjectController.java
@@ -2,14 +2,17 @@ package com;
 
 public class ProjectController {
 
-    // Рефакторинг: заменили громоздкий if-else на switch
     public void handleProjectAction(int actionCode) {
-        switch (actionCode) {
-            case 1 -> System.out.println("Creating project...");
-            case 2 -> System.out.println("Updating project...");
-            case 3 -> System.out.println("Deleting project...");
-            case 4 -> System.out.println("Viewing project stats...");
-            default -> System.out.println("Unknown action.");
+        if (actionCode == 1) {
+            System.out.println("Creating project...");
+        } else if (actionCode == 2) {
+            System.out.println("Updating project...");
+        } else if (actionCode == 3) {
+            System.out.println("Deleting project...");
+        } else if (actionCode == 4) {
+            System.out.println("Viewing project stats...");
+        } else {
+            System.out.println("Unknown action.");
         }
     }
 }
